04e15ecb65b516be193a942d621b5b0452b3c9bf
Xaver Lohmüller <xaver.lohmueller@me.com>
2019-01-25 15:07:00 +0100

    Add index html

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,13 +3,6 @@ import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-    /// Register providers first
-    try services.register(FluentSQLiteProvider())
-
-    /// Register routes to the router
-    let router = EngineRouter.default()
-    try routes(router)
-    services.register(router, as: Router.self)
 
     /// Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
